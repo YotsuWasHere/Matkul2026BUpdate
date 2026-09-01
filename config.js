279f883220c908528b89840946bf8ee0e1a8baa4
yotsuwashere <nova.risqy63@smk.belajar.id>
2026-09-01 10:15:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // JANGAN gunakan service_role / secret key di frontend.
 window.APP_CONFIG = {
   SUPABASE_ENABLED: true,
-  SUPABASE_URL: "https://knftfdstlmdtlzyhxpxh.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://knftfdstlmdtlzyhxpxh.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZnRmZHN0bG1kdGx6eWh4cHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyMjQ5ODAsImV4cCI6MjEwMzgwMDk4MH0.M3XOsudQ2nZoJ3dp0-j_FyppcANQvscOQrFUeRL71UY"
 };
